9a213702232963e30426e09acf2386c705bb26eb
Rolandos <rgeorgoulis@myseneca.ca>
2022-12-02 16:23:09 -0500

    moved index stuff over

--- src/index.js
+++ src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom/client';
 import 'normalize.css';
 import './index.css';
 import App from './App';
-import About from '../src/pages/About';
+
 
 import { AppProvider } from './context/appContext';
 
+import HomePage from "../src/pages/HomePage";
+import Error from "../src/pages/Error";
+import About from "../src/pages/About";
+import Portfolio from "../src/pages/Portfolio";
+
+export{
+    HomePage, About, Portfolio, Error
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
