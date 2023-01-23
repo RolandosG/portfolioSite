f31720997aaeac08e2e4b7f530ffb19e720a77a8
Rolandos <rgeorgoulis@myseneca.ca>
2023-01-23 17:00:59 -0500

    Links are now further from text

--- src/components/ProjectGrid.js
+++ src/components/ProjectGrid.js
@@ -44,9 +44,9 @@ const ProjectGrid = () => {
             <div class="card__back" style={{ background: 'radial-gradient(circle,#cee7a2,#a82e72)',
   backgroundSize: '240% 240%',
   animation: 'section-animation 10s ease infinite'}}><p 
-            class="card__body">A group project built with React to showcase abilities of full stack development
+            class="card__body">A group project built with React to showcase abilities of full stack development<p>
             <br></br><SiGithub  size={'20px'}/>&ensp;<a href="https://github.com/RolandosG/PRJ666-BlackJack-main" target="_blank" rel="noopener noreferrer">LEARN MORE</a>
-            <br></br><MdWebAsset size={'20px'}/>&ensp;<a href="https://blackjack.herokuapp.com/landing" target="_blank" rel="noopener noreferrer">Website</a></p>
+            <br></br><MdWebAsset size={'20px'}/>&ensp;<a href="https://blackjack.herokuapp.com/landing" target="_blank" rel="noopener noreferrer">Website</a></p></p>
           </div>
     </div>
     </div>
@@ -65,9 +65,9 @@ const ProjectGrid = () => {
             <div class="card__back"  style={{ background: 'radial-gradient(circle,#cee7a2,#a82e72)',
   backgroundSize: '240% 240%',
   animation: 'section-animation 10s ease infinite'}}><p 
-            class="card__body">A RESTful Applications backend that displays from mongoDB. 
+            class="card__body">A RESTful Applications backend that displays from mongoDB.<p>
               <br></br><SiGithub  size={'20px'}/>&ensp;<a href="https://github.com/RolandosG/backend" target="_blank" rel="noopener noreferrer">LEARN MORE</a>
-              <br></br><MdWebAsset size={'20px'}/>&ensp;<a href="https://storepage-back-end.herokuapp.com/products" target="_blank" rel="noopener noreferrer">Website</a></p>
+              <br></br><MdWebAsset size={'20px'}/>&ensp;<a href="https://storepage-back-end.herokuapp.com/products" target="_blank" rel="noopener noreferrer">Website</a></p></p>
           </div>
     </div>
     </div>
@@ -85,9 +85,9 @@ const ProjectGrid = () => {
             <div class="card__back" style={{background: 'radial-gradient(circle,#cee7a2,#a82e72)',
   backgroundSize: '240% 240%',
   animation: 'section-animation 10s ease infinite'}}><p 
-            class="card__body">Real time robot simulation using sensors to locate & move objects 
+            class="card__body">Real time robot simulation using sensors to locate & move objects<p>
             <br></br><SiGithub  size={'20px'}/>&ensp;<a href="https://github.com/RolandosG/exploration-machine" target="_blank" rel="noopener noreferrer">LEARN MORE</a>
-            <br></br><SiYoutube size={'20px'}/>&ensp;<a href="https://youtu.be/a51OEI5wD8A" target="_blank" rel="noopener noreferrer">Simulation</a></p>
+            <br></br><SiYoutube size={'20px'}/>&ensp;<a href="https://youtu.be/a51OEI5wD8A" target="_blank" rel="noopener noreferrer">Simulation</a></p></p>
           </div>
     </div>
     </div>
@@ -105,9 +105,10 @@ const ProjectGrid = () => {
             <div class="card__back" style={{background: 'radial-gradient(circle,#cee7a2,#a82e72)',
   backgroundSize: '240% 240%',
   animation: 'section-animation 10s ease infinite'}}><p 
-            class="card__body">A third person RPG adventure game made from scratch using unreals blueprints
+            class="card__body">A third person RPG adventure game made from scratch using unreals blueprints<p>
              <br></br><SiGithub  size={'20px'}/>&ensp;<a href="https://github.com/RolandosG/Rolos-Grand-Adventure" target="_blank" rel="noopener noreferrer">LEARN MORE</a>
               <br></br><SiYoutube size={'25px'}/>&ensp;<a href="https://youtu.be/GpPjLehmRSk" target="_blank" rel="noopener noreferrer">Gameplay</a>
+            </p>
             </p>
           </div>
     </div>
@@ -126,8 +127,8 @@ const ProjectGrid = () => {
           <div class="card__back" style={{background: 'radial-gradient(circle,#cee7a2,#a82e72)',
   backgroundSize: '240% 240%',
   animation: 'section-animation 10s ease infinite'}}><p 
-            class="card__body">The website I've designed here. <br></br><SiGithub  size={'20px'}/>&ensp;<a href="https://github.com/RolandosG/portfolioSite">
-           LEARN MORE</a></p>
+            class="card__body">The website I've designed here. <p><br></br><SiGithub  size={'20px'}/>&ensp;<a href="https://github.com/RolandosG/portfolioSite">
+           LEARN MORE</a></p></p>
           </div>
     </div>
     </div>
